bb984b68670267623eeb136acfb237ecc52cfde7
polarbear <15001725262@163.com>
2016-05-25 01:17:02 +0800

    second

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "knowhere"
+var unemployment: Double = 0.08
+
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights and \(unemployment) of unemployment"
+print(townDescription)
+
+
+
+
