a8a2f8fe869eb1452930549afe84ed080c01ce58
Kenny Huang <Fujun.Huang@Honeywell.com>
2016-05-25 16:48:44 +0800

    third commit

--- Conditionals.playground/Contents.swift
+++ Conditionals.playground/Contents.swift
@@ -10,4 +10,19 @@ if population < 10000 {
 } else {
     message = "\(population) is pretty big!"
 }
-        
+
+let y: Int8 = 120
+let z = y &+ 10
+
+let d1 = 1.1
+let d2: Double = 1.1
+let f1: Float = 100.3
+
+if d1 == d2 {
+    print("d1 and d2 are the same!")
+}
+
+print("d1+0.1 is \(d1+0.1)")
+if d1+0.1 == 1.2 {
+    print("d1 + 0.1 is equal to 1.2")
+}
